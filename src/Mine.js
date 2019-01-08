2daa5f4834bcec3f96c33fc7e4c450f1294c431b
masagus hariadi arief <adi.arief77@gmail.com>
2019-01-08 13:44:25 +0700

    child counter

--- src/Mine.js
+++ src/Mine.js
@@ -4,35 +4,44 @@ import Child from "./Child";
 class Mine extends React.Component {
   constructor() {
     super();
-    this.state = {
-      texts: [],
-      color: ""
-    };
+    this.state = { texts: [], color: "", counter: 0 };
+    this.changeColor = this.changeColor.bind(this);
+    this.counterClick = this.counterClick.bind(this);
   }
 
   // componentDidMount untuk set  nilai awal suatu state yang dimiliki oleh suatu component
   componentDidMount() {
     this.setState({
       texts: ["belajar react", "sungguh menyenangkan", "asik sekali"],
-      color: "blue"
+      color: "green"
     });
+  }
+
+  counterClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  changeColor() {
+    if (this.state.color === "green") {
+      this.setState({ color: "blue" });
+    } else {
+      this.setState({ color: "green" });
+    }
   }
 
   render() {
     let texts = this.state.texts.map(function(text) {
-      return <Child text={text} />;
-    });
+      return <Child text={text} counterClick={this.counterClick} />;
+    }, this);
 
     let style = { color: this.state.color };
 
     return (
-      <div style={style}>
+      <div style={style} onClick={this.changeColor}>
         <div id="mine"> Hello World </div>
         <div> This is me, React wkwkwkw</div>
-        <Child text="Belajar" />
-        <Child text="ReactJs" />
-        <Child text="Mantab" />
         {texts}
+        {this.state.counter}
       </div>
     );
   }
